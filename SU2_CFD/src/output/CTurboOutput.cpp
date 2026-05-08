251d6a9d80cd8c91a17c5b2486d4be9a2368f430
Rikymark <riccardo.gioia@polimi.it>
2026-05-08 13:29:18 +0200

    Corrected computation of T and Pt,ref

--- SU2_CFD/src/output/CTurboOutput.cpp
+++ SU2_CFD/src/output/CTurboOutput.cpp
@@ -67,6 +67,7 @@ void CTurbomachineryState::ComputeState(CFluidModel& fluidModel, const CTurbomac
   Entropy = fluidModel.GetEntropy();
   Enthalpy = fluidModel.GetStaticEnergy() + Pressure / Density;
   su2double soundSpeed = fluidModel.GetSoundSpeed();
+  Temperature = fluidModel.GetTemperature();
 
   /*--- Compute total TD quantities ---*/
   TotalEnthalpy = Enthalpy + 0.5 * GetVelocityValue() * GetVelocityValue();
@@ -84,7 +85,7 @@ void CTurbomachineryState::ComputeState(CFluidModel& fluidModel, const CTurbomac
   su2double tangVel2 = TangVelocity * TangVelocity;
   RelVelocity.assign(Velocity.begin(), Velocity.end());
   RelVelocity[1] -= TangVelocity;
-  su2double relVel2 = GetRelVelocityValue();
+  su2double relVel2 = GetRelVelocityValue()*GetRelVelocityValue();
   FlowAngle = atan(RelVelocity[1] / RelVelocity[0]);
   RelMach.assign(RelVelocity.begin(), RelVelocity.end());
   std::for_each(RelMach.begin(), RelMach.end(), [&](su2double& el) { el /= soundSpeed; });
@@ -127,6 +128,7 @@ void CTurbineBladePerformance::ComputePerformance(const CTurbomachineryCombinedP
   TotalPressureLoss = (InletState.GetTotalRelPressure() - OutletState.GetTotalRelPressure()) /
                       (OutletState.GetTotalRelPressure() - OutletState.GetPressure());
   KineticEnergyLoss = 2 * (OutletState.GetEnthalpy() - enthalpyOutIs) / relVelOutIs2;
+
 }
 
 CCompressorBladePerformance::CCompressorBladePerformance(CFluidModel& fluidModel, unsigned short nDim, su2double areaIn,
